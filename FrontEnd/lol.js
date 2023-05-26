943079d93279f109ed642dc70014c75eadadf9b1
huss <46470134+hasshuss@users.noreply.github.com>
2023-05-26 11:00:09 +0200

    Add files via upload
    affichage gallerie

--- FrontEnd/lol.js
+++ FrontEnd/lol.js
@@ -1,11 +1,35 @@
-fetch('http://localhost:5678/api/works')
-  .then(function(reponse) {
-    return reponse.json();
-  })
-  .then(function(body) {
-    console.log(body);
-  })
-  .catch(function(error) {
-    console.error('Erreur:', error);
-  }); 
-  console.log(body);
+async function getWork() {
+  try {
+      const response = await fetch('http://localhost:5678/api/works');
+      
+      if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+      } else {
+          const workData = await response.json();
+          sessionStorage.setItem('works', JSON.stringify(workData));
+      }
+  } catch (error) {
+      console.error('Une erreur est survenue lors de la récupération des données : ', error);
+  }
+}
+
+getWork().then(() => {
+  let travaux = JSON.parse(sessionStorage.getItem('works'));
+  console.log(travaux);
+  let i = 0;
+  let gallery = document.getElementById('gallery');
+  while (i < travaux.length) {
+    let figure = document.createElement('figure');
+    let image = document.createElement('img');
+    image.src = travaux[i].imageUrl;
+    figure.appendChild(image);
+    let figcaption = document.createElement('figcaption');
+    figcaption.textContent = travaux[i].title;
+    figure.appendChild(figcaption);
+    gallery.appendChild(figure);
+    i++;
+  }
+
+  
+});
+
